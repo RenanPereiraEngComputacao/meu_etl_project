1f4e2e0ae756285b4414df71b3e5a219815ea948
Renan Pereira <ysaky007@gmail.com>
2025-06-24 17:47:36 -0300

    ajuste env para novo banco

--- DBconect/mysql_conn.py
+++ DBconect/mysql_conn.py
@@ -13,8 +13,23 @@ def get_mysql_connection():
             password=os.getenv("MYSQL_PASSWORD"),
             database=os.getenv("MYSQL_DATABASE")
         )
-        print("✅ Conexão estabelecida com sucesso MySQL.")
+        print(f"✅ Conexão estabelecida com sucesso MySQL: {os.getenv('MYSQL_DATABASE')}")
         return conn
+    except Exception as e:
+        print(f"Erro na conexão com MySQL: {e}")
+        raise
+
+def get_mysql_connection2():
+    try:
+        conn2 = mysql.connector.connect(
+            host=os.getenv("MYSQL_HOST"),
+            port=os.getenv("MYSQL_PORT"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database=os.getenv("MYSQL_DATABASE2")
+        )
+        print(f"✅ Conexão estabelecida com sucesso MySQL: {os.getenv('MYSQL_DATABASE2')}")
+        return conn2
     except Exception as e:
         print(f"Erro na conexão com MySQL: {e}")
         raise
