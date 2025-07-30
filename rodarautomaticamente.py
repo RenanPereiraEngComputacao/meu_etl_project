536ff6d4e69a14833d388727adcb98324383dec5
Renan Pereira <ysaky007@gmail.com>
2025-07-30 16:44:42 -0300

    Update rodarautomaticamente.py

--- rodarautomaticamente.py
+++ rodarautomaticamente.py
@@ -1,13 +1,34 @@
 import time
 import subprocess
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+from DBconect.postgres_conn import get_postgres_connection
+import contextlib
+
+load_dotenv()
 
 print("Iniciando o script rodarautomaticamente.py...")
 
+def tem_pedidos_nao_sincronizados():
+    try:
+        conn = get_postgres_connection()
+        with contextlib.closing(conn.cursor()) as cursor:
+            cursor.execute("SELECT 1 FROM orders WHERE statussincronismo = false LIMIT 1")
+            resultado = cursor.fetchone()
+        conn.close()
+        return resultado is not None
+    except Exception as e:
+        print(f"Erro ao verificar pedidos não sincronizados: {e}")
+        return False
+
 def executar_script_pedido():
-    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Iniciando execução de sync_order.py")
-    subprocess.run(["python", "c:/meu_etl_project/sync_order.py"])
-    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Finalizou execução de sync_order.py\n")
+    if tem_pedidos_nao_sincronizados():
+        print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Iniciando execução de sync_order.py")
+        subprocess.run(["python", "c:/meu_etl_project/sync_order.py"])
+        print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Finalizou execução de sync_order.py\n")
+    else:
+        print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Nenhum pedido pendente. Ignorando sync_order.py.")
 
 def executar_script():
     print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Iniciando execução de att_estoque.py")
